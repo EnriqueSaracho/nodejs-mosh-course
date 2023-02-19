1fce5ccf358ecce316572954e05128ad8ea4c47f
Enrique Saracho Felix <esarachof97@gmail.com>
2023-02-18 19:22:36 -0800

    Modified app9.js

--- app9.js
+++ app9.js
@@ -3,11 +3,13 @@ const EventEmitter = require("node:events"); // Imports `EventEmitter` class
 const emitter = new EventEmitter(); // Making an instance of the class
 
 // Register a listener
-emitter.on("messageLoaded", function () {
-  console.log("Listener called");
+emitter.on("messageLoaded", (arg) => {
+  console.log("Listener called", arg);
 });
 
 // The object created has all the properties of the class
-emitter.emit("messageLoaded"); // Raise event
+emitter.emit("messageLoaded", { id: 1, url: "http//" }); // Raise event
+
+// The object inside the emit method is called `Event Argument` and it can be passed on to the listener
 
 // The listener must be written before the Raise event
